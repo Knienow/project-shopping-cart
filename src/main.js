dc85a3cc8f7748696bd988c8957317e5d352231e
Katia Nienow <nienowkatia@gmail.com>
2023-02-14 20:55:15 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-fetchProductsList('computador').then((results) => console.log(results));
+fetchProductsList('computador').then((data) => console.log(data));
+
+// pegar o elemento html <section class="products">
+const sectionProducts = document.querySelector('.products');
+
+const products = async () => {
+  const productList = await fetchProductsList('computador');
+  const list = productList.forEach((product) => {
+    sectionProducts.appendChild(createProductElement(product));
+  });
+  return list;
+};
+
+products();
+// .catch(() => {
+//   console.log('Algum erro ocorreu, recarregue a página e tente novamente');
+// });
