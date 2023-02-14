43ab3aac4cbb4d0f06918c95d7379ffc22917b73
Katia Nienow <nienowkatia@gmail.com>
2023-02-14 17:14:47 -0300

    primeira versão dos testes do requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,38 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
+    expect(typeof fetchProductsList()).toBe('function');
     
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
 
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
 
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const result = await fetchProductsList('computador');
+
+    expect(result).toEqual(computadorSearch);
+  });
+
+  it('ao chamar a função fetchProductsList sem atribuir parâmetros, deve retornar um erro com a mensagem "Termo de busca não informado"', async () => {
+    const emptyParameter = '';
+  
+    await expect(fetchProductsList(emptyParameter)).rejects.toThrow(
+      new Error('Termo de busca não informado')
+    );
+  });
+});
 
   // it('...', () => {
   // });
-});
+
+  //
