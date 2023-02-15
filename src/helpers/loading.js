286f3b84d8a100926b900398cdd0f4bab1fa6c1d
Katia Nienow <nienowkatia@gmail.com>
2023-02-15 19:43:06 -0300

    requisito 4

---
+++ src/helpers/loading.js
@@ -0,0 +1,14 @@
+// export const showLoading = () => {
+//   const h2 = document.createElement('h2');
+//   h2.innerText = 'Carregando...';
+//   h2.className('loading');
+//   document.body.appendChild(h2);
+//   return h2;
+// };
+
+// export const hideLoading = () => {
+//   const loadings = document.querySelector('loading');
+//   if (loadings.lentgh) {
+//     loadings[0].remove();
+//   }
+// };
--- src/main.js
+++ src/main.js
@@ -1,24 +1,40 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
+// import { showLoading, hideLoading } from './helpers/loading';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 fetchProductsList('computador').then((data) => console.log(data));
 
-// pegar o elemento html <section class="products">
 const sectionProducts = document.querySelector('.products');
+
+const showLoading = () => {
+  const h2 = document.createElement('h2');
+  h2.innerText = 'Carregando...';
+  h2.classList.add('loading');
+  document.body.appendChild(h2);
+};
+
+const hideLoading = () => {
+  const loadings = document.querySelector('.loading');
+  loadings.remove();
+};
+
+showLoading();
 
 const products = async () => {
   const productList = await fetchProductsList('computador');
   const list = productList.forEach((product) => {
     sectionProducts.appendChild(createProductElement(product));
   });
+  hideLoading();
   return list;
 };
 
 products();
+
 // .catch(() => {
 //   console.log('Algum erro ocorreu, recarregue a página e tente novamente');
 // });
